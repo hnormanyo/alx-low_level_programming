d6e4e174e67d48fe5a42408a24d2f9eb8e441049
hnormanyo <Henrynormanyo2@gmail.com>
2022-09-09 04:57:03 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,12 @@
 #include <unistd.h>
 
 /**
- *main - prints to string
- *Description: Prints "and that piece of art is useful.." 
- *Return: (1) successful
+ * main-pritns to string
+ *"and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * Return: 1
  */
-
- int main(void)
- { 
- write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
- return (1);
- } 
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
