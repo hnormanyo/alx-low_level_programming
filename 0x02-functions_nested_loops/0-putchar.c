362533d4463936714a3ef0ab3a125412f4ee8a07
hnormanyo <Henrynormanyo2@gmail.com>
2022-09-13 15:00:49 +0000

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+# include "main.h"
+/**
+  *  main - Entry point
+  * Description: a program that prints _putchar followed by new line
+  * Return: Always 0 (success)
+  * main - Prints _putchar as a message.
+  */
+int main(void)
+{
+	pritnf("_putchar\n");
+	return (0);
+}
