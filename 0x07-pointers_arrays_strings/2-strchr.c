b88356c9059a006c1543b699549deaec11938e19
hnormanyo <Henrynormanyo2@gmail.com>
2022-09-26 19:49:31 +0000

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,12 +7,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	nt i = 0;
 
-	for (; s[i] >= '\0'; i++)
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
-			return (&s[i]);
+			return (s + i);
 	}
-	return (0);
+	return ('\0');
+
 }
